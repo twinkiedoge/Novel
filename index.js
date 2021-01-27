e6723c8b92e533f45c3bb739c3645661c08aaacd
twinkiedoge <abobroff8@gmail.com>
2021-01-26 23:09:28 -0500

    .

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ var idea = document.getElementById("idea");
 function submit(){
 
     var rootRef = firebase.database().ref();
-
     var ideaVal = idea.value;
-
     rootRef.push().set(ideaVal);
     window.alert("FB");
+}
+
+function pullIdea(){
+    window.alert("test");
+    var rootRef = firebase.database().ref();
+
+
 }
