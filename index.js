2f88e73092236dcc9e16013c9498264d7fdf5cd3
twinkiedoge <abobroff8@gmail.com>
2021-01-26 22:46:14 -0500

    .

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ var submitButton = document.getElementById("submitButton");
 var idea = document.getElementById("idea");
 
 function submit(){
-<<<<<<< HEAD
-  var rootRef = firebase.database().ref();
-  rootRef.child("text").set("weiner");
-=======
 
-  var rootRef = firebase.database().ref();
+    var rootRef = firebase.database().ref();
 
-  var ideaVal = idea.value;
+    var ideaVal = idea.value;
 
-  rootRef.push().set(ideaVal);
->>>>>>> d0cd5ad1299242789a26e145bbb6522b6578d1da
-  window.alert("FB");
+    rootRef.push().set(ideaVal);
+    window.alert("FB");
 }
