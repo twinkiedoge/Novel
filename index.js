88d66810d0c1939213aa1e8cf735df3d0aa4d09b
ColbyCho <colby.cho@tufts.edu>
2021-01-26 22:45:04 -0500

    d

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var submitButton = document.getElementById("submitButton");
 var idea = document.getElementById("idea");
 
 function submit(){
-  window.alert("FB");
   var rootRef = firebase.database().ref();
   rootRef.child("text").set("weiner");
-
+  window.alert("FB");
 }
