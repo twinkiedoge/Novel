20a6e7e11617b1a510caf379f3ad1f133e082087
twinkiedoge <abobroff8@gmail.com>
2021-01-26 22:28:06 -0500

    j

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var submitButton = document.getElementById("submitButton");
 var idea = document.getElementById("idea");
 
 function submit(){
+  window.alert("FB");
   var rootRef = firebase.database().ref();
   rootRef.child("text").set("weiner");
-  window.alert("FB");
+
 }
